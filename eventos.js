870edaf6e51334ea92c0925eb8c4e008a983a261
Suleidis Leyva <suleidisdanai@gmail.com>
2024-08-13 12:09:15 -0300

    solucion-click

--- eventos.js
+++ eventos.js
@@ -1,4 +1,10 @@
-const divisor = document.querySelector('div')
-divisor.addEventListener('click',  () =>{
-    alert('Hola! Soy el div')
-})
+const divisor = document.querySelector('div');
+divisor.addEventListener('click', () => {
+    alert('Hola! Soy el div');
+});
+
+const boton = document.getElementById('saludarBtn');
+boton.addEventListener('click', (event) => {
+    event.stopPropagation();
+    alert('Hola!😊🙌');
+});
